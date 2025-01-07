b8624e997db90ef3b2050a1f804ecc7cdda978fd
ssogal00 <playstationfifa83@gmail.com>
2025-01-07 18:11:27 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,61 @@
 #include <iostream>
 #include "cuda_kernel.cuh"
 #include "SDL2/SDL.h"
+#include "Complex.h"
+#include <cmath>
+#include <complex>
+
+std::complex<double> c(-0.7, 0.27015);  // You can change this constant to get different Julia sets
+
+const int WIDTH = 640;
+const int HEIGHT = 480;
+
+const int MAX_ITERATIONS = 100;
+
+uint32_t julia(double x, double y) 
+{
+    std::complex<double> z(x, y);
+    int iterations = 0;
+
+    while (std::abs(z) < 2 && iterations < MAX_ITERATIONS) 
+    {
+        z = z * z + c;
+        iterations++;
+    }
+
+    if (iterations == MAX_ITERATIONS) return 0;
+
+    return static_cast<uint32_t>(iterations * 255 / MAX_ITERATIONS);
+}
+
+void renderJuliaSet(SDL_Renderer* renderer) 
+{
+    for (int y = 0; y < HEIGHT; ++y) 
+    {
+        for (int x = 0; x < WIDTH; ++x) 
+        {
+            double real = (x - WIDTH / 2.0) * 4.0 / WIDTH;
+            double imag = (y - HEIGHT / 2.0) * 4.0 / HEIGHT;
+
+            uint32_t color = julia(real, imag);
+
+            SDL_SetRenderDrawColor(renderer, color, color, color, 255);
+            SDL_RenderDrawPoint(renderer, x, y);
+        }
+    }
+}
+
 
 int main() 
 {
+	Complex4 a(1, 2, 3, 4, 1,2,3,4);
+
+    Complex4 b(1, 2, 3, 4, 1, 2, 3, 4);
+
+	Complex4 c = a * b;
+
+	PrintM256(c.mValue);
+
     /*const int arraySize = 5;
     const int a[arraySize] = { 1, 2, 3, 4, 5 };
     const int b[arraySize] = { 10, 20, 30, 40, 50 };
@@ -25,7 +77,8 @@ int main()
     */
 
     // SDL 초기화
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) 
+    {
         std::cerr << "SDL 초기화 실패: " << SDL_GetError() << std::endl;
         return 1;
     }
@@ -40,14 +93,41 @@ int main()
         SDL_WINDOW_SHOWN                // 윈도우 플래그
     );
 
-    if (window == nullptr) {
+    if (window == nullptr) 
+    {
         std::cerr << "윈도우 생성 실패: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
-    // 윈도우를 3초 동안 유지
-    SDL_Delay(3000);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	if (renderer == nullptr) 
+    {
+		std::cerr << "렌더러 생성 실패: " << SDL_GetError() << std::endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
+
+    bool quit = false;
+    SDL_Event event;
+
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        renderJuliaSet(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
 
     // 정리
     SDL_DestroyWindow(window);
